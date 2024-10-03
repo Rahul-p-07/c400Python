6b3a5976c11a0a9e2a7283725da4bd9121ae480e
rahul-p-07 <rahulp0702@gmail.com>
2024-10-03 12:04:16 +0530

    Updated Python Script

--- program.py
+++ program.py
@@ -3,7 +3,7 @@ taxizone=requests.get("https://d37ci6vzurychx.cloudfront.net/misc/taxi_zone_look
 #print(taxizone.text)
 records=taxizone.text
 rec=records.split("\n")[1:266]
-file= open("taxi_zone_lookup","w")
+file= open("taxi_zone_lookup.txt","w")
 x=len(rec)
 borough=[]
 c=0
