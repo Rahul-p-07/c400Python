e33966f89a6d54adc64aee27742d3c85ef95d014
rahul-p-07 <rahulp0702@gmail.com>
2024-10-03 11:23:02 +0530

    Updated Python Script

--- program.py
+++ program.py
@@ -3,7 +3,7 @@ taxizone=requests.get("https://d37ci6vzurychx.cloudfront.net/misc/taxi_zone_look
 #print(taxizone.text)
 records=taxizone.text
 rec=records.split("\n")[1:266]
-file= open("./taxi_zone_lookup","w")
+file= open("taxi_zone_lookup","w")
 x=len(rec)
 borough=[]
 c=0
@@ -11,8 +11,9 @@ for i in rec:
     y=i.split(",")[1]
     if y not in borough:
         borough.append(y)
-    if y=="Brooklyn":
+    if y=='"Brooklyn"':
         c+=1
+
 fin=sorted(borough)
 file.write(f'''The total number of records are: {x}
 The Unique Boroughs are: {fin}
